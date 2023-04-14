af388e5cf36de8ddb9d86f13d3ace40d09235ef5
Nicholas DeMarinis <ndemarinis@wpi.edu>
2023-04-14 06:04:28 -0400

    Initial commit.

---
+++ test_index.py
@@ -0,0 +1,27 @@
+import pytest
+from index import Indexer
+
+'''
+Uncomment when ready to use!
+
+def file_as_set(filename):
+    """
+    Returns all of the non-empty lines in the file, as strings in a set.
+    """
+    line_set = set()
+    with open(filename, "r") as file:
+        line = file.readline()
+        while line and len(line.strip()) > 0:
+            line_set.add(line.strip())
+            line = file.readline()
+    return line_set
+
+def test_file_contents():
+    simple_index = Indexer("wikis/SimpleWiki.xml", "simple_titles.txt",
+       "simple_docs.txt", "simple_words.txt")
+    simple_index.run() # run the indexer to write to the files
+    titles_contents = file_as_set("simple_titles.txt")
+    assert len(titles_contents) == 2
+    assert "200::Example page" in titles_contents
+    assert "30::Page with links" in titles_contents
+'''
